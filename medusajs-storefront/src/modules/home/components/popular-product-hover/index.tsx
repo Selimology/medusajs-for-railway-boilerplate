8462171495faa701549405337eec2e197022eb33
Kamil Ertekin <kertekin@entos.dev>
2024-07-08 18:21:53 +0200

    feat: quick fix

--- medusajs-storefront/src/modules/home/components/popular-product-hover/index.tsx
+++ medusajs-storefront/src/modules/home/components/popular-product-hover/index.tsx
@@ -1,48 +1,48 @@
-// "use client"
+"use client"
 
-// import LocalizedClientLink from "@modules/common/components/localized-client-link"
-// import Image from "next/image"
-// import { LookBookType } from "types/lookbook"
+import LocalizedClientLink from "@modules/common/components/localized-client-link"
+import Image from "next/image"
+import { LookBookType } from "types/lookbook"
 
-// interface PopularProductHoverProps {
-//   productData: LookBookType
-// }
+interface PopularProductHoverProps {
+  productData: LookBookType
+}
 
-// const LookBookHover = ({ productData }: PopularProductHoverProps) => {
-//   return (
-//     <>
-//       <Image
-//         src={productData.image.imageUrl}
-//         width={1000}
-//         height={1000}
-//         alt={productData.image.alt}
-//         priority={true}
-//         className="lg:hw-full h-full object-cover"
-//       />
-//       {productData.products.map((product) => (
-//         <div
-//           key={product.id}
-//           className="dots absolute cursor-pointer"
-//           style={{ top: product.dotTop, left: product.dotLeft }}
-//           cursor-pointer
-//         >
-//           <div className="top-dot w-8 h-8 rounded-full bg-outline flex items-center justify-center">
-//             <span className="bg-white w-3 h-3 rounded-full duration-300"></span>
-//           </div>
-//           <LocalizedClientLink
-//             className="product-infor bg-white rounded-2xl p-4"
-//             href={product.url}
-//           >
-//             <div className="text-title name">{product.name}</div>
-//             <div className="price text-center">${product.price}</div>
-//             <div className="text-center underline mt-1 text-button-uppercase duration-300 text-secondary2 hover:text-black">
-//               View
-//             </div>
-//           </LocalizedClientLink>
-//         </div>
-//       ))}
-//     </>
-//   )
-// }
+const LookBookHover = ({ productData }: PopularProductHoverProps) => {
+  return (
+    <>
+      <Image
+        src={productData.image.imageUrl}
+        width={1000}
+        height={1000}
+        alt={productData.image.alt}
+        priority={true}
+        className="lg:hw-full h-full object-cover"
+      />
+      {/* {productData.products.map((product) => (
+        <div
+          key={product.id}
+          className="dots absolute cursor-pointer"
+          style={{ top: product.dotTop, left: product.dotLeft }}
+          cursor-pointer
+        >
+          <div className="top-dot w-8 h-8 rounded-full bg-outline flex items-center justify-center">
+            <span className="bg-white w-3 h-3 rounded-full duration-300"></span>
+          </div>
+          <LocalizedClientLink
+            className="product-infor bg-white rounded-2xl p-4"
+            href={product.url}
+          >
+            <div className="text-title name">{product.name}</div>
+            <div className="price text-center">${product.price}</div>
+            <div className="text-center underline mt-1 text-button-uppercase duration-300 text-secondary2 hover:text-black">
+              View
+            </div>
+          </LocalizedClientLink>
+        </div>
+      ))} */}
+    </>
+  )
+}
 
-// export default LookBookHover
+export default LookBookHover
